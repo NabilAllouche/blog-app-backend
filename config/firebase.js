ab30924354daf6e6f306ea7b8ed17380d9b3978b
NabilAllouche <nabilallouche49@gmail.com>
2024-01-03 15:02:28 +0100

    ADD envirement variables

--- config/firebase.js
+++ config/firebase.js
@@ -8,13 +8,13 @@ import multer from "multer";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 export const firebaseConfig = {
-  apiKey: "AIzaSyCkWeW1ZfJRxpe44digl4cMhux718cJK8c",
-  authDomain: "personal-project-nabil.firebaseapp.com",
-  projectId: "personal-project-nabil",
-  storageBucket: "personal-project-nabil.appspot.com",
-  messagingSenderId: "503369652011",
-  appId: "1:503369652011:web:5be7420274fd63db86976e",
-  measurementId: "G-BYZP72B91V",
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECY_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
+  measurementId: process.env.MEASUREMENT_ID,
 };
 
 export const upload = multer({ storage: multer.memoryStorage() });
