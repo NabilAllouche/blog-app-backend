0804fde6ebf83c42fab516bc169534c2b0537914
NabilAllouche <nabilallouche49@gmail.com>
2024-01-03 15:21:54 +0100

    remove bug

--- config/firebase.js
+++ config/firebase.js
@@ -1,6 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import multer from "multer";
+import dotenv from "dotenv";
+dotenv.config();
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
